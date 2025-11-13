a32c59797a276632cc4a9b4145e462a655969c23
Dilini <dilini@knightowl.online>
2025-11-13 12:29:14 +0530

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Home from "./components/Home/page";
 import Patners from "./components/Patners/page";
 import Article from "./components/Article/page";
 import Initiative from "./components/Initiative/page";
 import Testimonials from "./components/Testimonials/page";
 import Faq from "./components/FaQ/page";
 import HeroBanner from "./components/HeroBanner/page";
+import Hero from "./components/Hero/page";
 
  
 
 export default function Page() {
   return (
     <>
-    <Home />
+    <Hero />
     <Patners />
     <Article />
     <Initiative />
